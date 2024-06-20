15e284a7c439849bffa4cdd0cb900aa6c5c35af7
THEOD0R1 <squirrelnub@gmail.com>
2024-06-20 15:08:38 +0200

    test v2

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -21,7 +21,7 @@ export const Header = () => {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           {isSupabaseConnected && <AuthButton />}
-          <Navigate navigateTo="./checkout">Go to checkout</Navigate>
+          <Navigate navigateTo="/checkout">Go to checkout</Navigate>
         </div>
       </nav>
     </div>
