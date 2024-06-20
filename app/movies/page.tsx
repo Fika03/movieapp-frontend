63974d6dbef988cb1596e27e68ac1e56c95d416e
THEOD0R1 <squirrelnub@gmail.com>
2024-06-20 17:01:22 +0200

    added redirect to protected page if loged in

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,6 +1,18 @@
 import { SelectMoviePage } from "@/components/MovieComponents/pagination/SelectMoviePage";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default async function Main() {
+export default async function Movies() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect("/protected");
+  }
+
   return (
     <div className=" w-full flex flex-col  items-center">
       <SelectMoviePage />
