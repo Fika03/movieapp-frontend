38d526c072887e299a4cbde215376f95a7c837a2
THEOD0R1 <squirrelnub@gmail.com>
2024-06-21 13:45:40 +0200

    deleted search params in succeeded

--- app/checkout/succeeded/page.tsx
+++ app/checkout/succeeded/page.tsx
@@ -1,20 +1,3 @@
-"use client";
-
-import { Loading } from "@/components/Loading";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
-
 export default function checkoutSucceeded() {
-  const searchParams = useSearchParams();
-
-  const paymentDescription = searchParams.get("payment_intent");
-
-  return (
-    <Suspense fallback={<Loading />}>
-      <section>
-        <h1>succeeded</h1>
-        <span>Your payment Id is: {paymentDescription} </span>
-      </section>
-    </Suspense>
-  );
+  return <h1>succeeded</h1>;
 }
