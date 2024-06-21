b7c3b06ac3554ee572a8045ab61b8401c09fe7f8
THEOD0R1 <squirrelnub@gmail.com>
2024-06-21 13:32:41 +0200

    test

--- app/checkout/succeeded/page.tsx
+++ app/checkout/succeeded/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function () {
   const searchParams = useSearchParams();
 
-  const paymentDescription = searchParams.get("payment_intent") || " ";
+  const paymentDescription = searchParams.get("payment_intent");
 
   return (
     <section>
       <h1>succeeded</h1>
-      <span>Your payment Id is: {paymentDescription} </span>
+      <Suspense>
+        <span>Your payment Id is: {paymentDescription} </span>
+      </Suspense>
     </section>
   );
 }
